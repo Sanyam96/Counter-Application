2dbf03f13352e76817553f8c41a725ef4c4dcd8e
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-02-09 17:27:40 +0530

    Added Switch + basic Functionality

--- app/src/main/java/com/example/sanyam/counterapplication/MainActivity.java
+++ app/src/main/java/com/example/sanyam/counterapplication/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.sanyam.counterapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch s;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView2);
+        s = (Switch) findViewById(R.id.switch1);
+
+        s.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( s.isChecked() ){
+                    s.setText("Enabled");
+                }
+                else{
+                    s.setText("Disabled");
+                }
+
+            }
+        });
+
     }
 }
